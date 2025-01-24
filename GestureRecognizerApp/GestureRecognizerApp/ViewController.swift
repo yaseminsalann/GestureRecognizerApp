3c028a1570bf45ef7a0f5d95b7c30ec194eb1cb9
yaseminsalann <yaseminsalann@gmail.com>
2025-01-24 17:57:32 +0300

    GestureRecognizer
    GestureRecognizer

---
+++ GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizerApp
+//
+//  Created by Yasemin salan on 24.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GestureRecognizerApp/GestureRecognizerAppTests/GestureRecognizerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestureRecognizerAppTests.swift
+//  GestureRecognizerAppTests
+//
+//  Created by Yasemin salan on 24.01.2025.
+//
+
+import Testing
+@testable import GestureRecognizerApp
+
+struct GestureRecognizerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
