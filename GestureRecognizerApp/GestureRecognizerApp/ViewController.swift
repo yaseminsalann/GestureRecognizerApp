3d987584277cd92465264c2f11ae839fc1098eeb
yaseminsalann <yaseminsalann@gmail.com>
2025-01-26 17:01:06 +0300

    UIRotationGestureRecognizer
    UIRotationGestureRecognizer eklendi.

--- GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -42,13 +42,48 @@ class ViewController: UIViewController {
         // UIPinchGestureRecognizer oluştur ve hedef aksiyon ekle
         let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
         imageView.addGestureRecognizer(pinchGesture)
+        /*
+         UIPinchGestureRecognizer Oluşturma:
+         Gesture Recognizer, hedef bir aksiyonu (metodu) çağırır.
+         target: Gesture hareketinin tetikleyeceği sınıf.
+         action: Kıstırma hareketini işlemek için çağrılan metod.
+         Gesture'ı Bir Görünümde Kullanma:
+         Gesture Recognizer, bir UIView veya alt sınıflarına (örneğin UIImageView) eklenir.
+         isUserInteractionEnabled = true ayarı, UIView'ın kullanıcı etkileşimlerini algılayabilmesi için önemlidir.
+         sender.scale:
+         Kıstırma hareketinin ölçeğini temsil eder. Varsayılan değeri 1.0'dır.
+         Bu değeri her hareket sırasında uygulayarak görünümün boyutunu dinamik olarak değiştirebilirsiniz.
+         sender.scale = 1.0 ile hareketi sıfırlayarak sürekli büyüme/küçülme engellenir.
+         */
     }
     
     @IBAction func rotationGestureRecognizerButton(_ sender: Any) {
+        // Görüntü oluştur ve ekle
+        imageView = UIImageView(image: UIImage(named: "sakura"))
+        imageView.isUserInteractionEnabled = true // Gesture'ları algılamak için gerekli
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: 100, y: 200, width: 200, height: 200)
+        view.addSubview(imageView)
+
+        // UIRotationGestureRecognizer oluştur ve hedef aksiyon ekle
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotation(_:)))
+        imageView.addGestureRecognizer(rotationGesture)
+        
+        /*
+         UIRotationGestureRecognizer Tanımlama:
+         UIRotationGestureRecognizer, hedef aksiyon (metod) ve hedef nesneyle (target) ilişkilendirilir.
+         sender.rotation: Kullanıcının döndürme açısını temsil eder (radyan cinsinden).
+         Dönüşü Uygulama:
+         view.transform.rotated(by:): Döndürme işlemi, mevcut dönüş açısına göre güncellenir.
+         sender.rotation = 0: Gesture'ın her hareket sonrası sıfırlanması sağlanır, böylece döndürme işlemi birikmez.
+         isUserInteractionEnabled:
+         Gesture'ların çalışabilmesi için ilgili görünümün isUserInteractionEnabled özelliği true olmalıdır.
+         */
     }
     
     
     @IBAction func swipeGestureRecognizerButton(_ sender: Any) {
+        
     }
     
     @IBAction func panGestureRecognizerButton(_ sender: Any) {
@@ -83,5 +118,14 @@ class ViewController: UIViewController {
           // Gesture'ın ölçek değerini sıfırla
           sender.scale = 1.0
       }
+    @objc func handleRotation(_ sender: UIRotationGestureRecognizer) {
+           guard let view = sender.view else { return }
+
+           // Görüntüyü döndür
+           view.transform = view.transform.rotated(by: sender.rotation)
+
+           // Gesture'ın rotation değerini sıfırla
+           sender.rotation = 0
+       }
 }
 
