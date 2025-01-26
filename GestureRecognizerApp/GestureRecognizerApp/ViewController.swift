03100b47b68bea316a98c17475c6a73b1893cc89
yaseminsalann <yaseminsalann@gmail.com>
2025-01-26 16:25:04 +0300

    UITapGestureRecognizer
    UITapGestureRecognizer: Bir veya daha fazla dokunma ile tıklama hareketini algılar.Uygulama içindeki örneği tek tıklama dır.

--- GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
+++ GestureRecognizerApp/GestureRecognizerApp/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var isSakura = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func tapGestureRecognizerButton(_ sender: Any) {
+        //görselin tıklanabilir olmasını sağlıyoruz
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+    }
+    @objc func changeImage() {
+        //görsele tıklandığında changeImage fonksiyonu çalışır.
+        
+        //tıklandığında resimler değişmesi için kontrol eklendi.
+        if isSakura {
+            isSakura = false
+            imageView.image = UIImage(named: "sakura")
+            myLabel.text = "SAKURA"
+        } else {
+            isSakura = true
+            imageView.image = UIImage(named: "sakuralar")
+            myLabel.text = "SAKURALAR"
+        }
+    }
 
 }
 
